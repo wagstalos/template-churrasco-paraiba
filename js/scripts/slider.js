1de0d60d531f928bd711927ec0556d7c5c838e90
Wagner Paulo <wagstalos@hotmail.com>
2025-01-27 20:36:52 -0300

    feat: mobile adjust

--- js/scripts/slider.js
+++ js/scripts/slider.js
@@ -1,24 +1,42 @@
 // var swiper = new Swiper(".mySwiper", {
-//   rewind: true,
+//   effect: "cube",
+//   grabCursor: true,
+//   cubeEffect: {
+//     shadow: true,
+//     slideShadows: true,
+//     shadowOffset: 20,
+//     shadowScale: 0.94,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
 //   navigation: {
 //     nextEl: ".swiper-button-next",
 //     prevEl: ".swiper-button-prev",
 //   },
 // });
+
 var swiper = new Swiper(".mySwiper", {
-  effect: "cube",
-  grabCursor: true,
-  cubeEffect: {
-    shadow: true,
-    slideShadows: true,
-    shadowOffset: 20,
-    shadowScale: 0.94,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 1.1,
+      spaceBetween: 10,
+    },
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
   },
 });
